48aff001b4b75d0cedf5329f972df897c351113e
resparzasoto <rubenesparzasoto95@gmail.com>
2020-09-15 21:19:53 -0600

    Manejo de archivos estáticos

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
